bbd22921feecbb80471e3a3f4e74e05316abe183
Eduardo Serrano <eduardomserrano@outlook.com>
2022-09-18 18:49:19 +0100

    adding tests 5

--- ShareJobsData/tests/ShareJobsDataCli.Tests/CliIntegration/ReadDataDifferentWorkflow/CliIntegrationTests.cs
+++ ShareJobsData/tests/ShareJobsDataCli.Tests/CliIntegration/ReadDataDifferentWorkflow/CliIntegrationTests.cs
@@ -97,10 +97,90 @@ public class CliIntegrationTests
     }
 
     /// <summary>
-    /// Tests that the --artifact-name option is must have a value for the <see cref="ReadDataFromDifferentGitHubWorkflowCommand"/> command.
+    /// Tests the validation of the --auth-token option for the <see cref="ReadDataFromDifferentGitHubWorkflowCommand"/> command.
     /// </summary>
-    [Fact]
-    public async Task ArtifactNameMustHaveValue()
+    [Theory]
+    [InlineData("empty-string", "")]
+    [InlineData("white-space", "   ")]
+    public async Task AuthTokenValidation(string scenario, string authToken)
+    {
+        using var console = new FakeInMemoryConsole();
+        var app = new ShareDataBetweenJobsCli();
+        app.CliApplicationBuilder.UseConsole(console);
+        var args = new[]
+        {
+            "read-data-different-workflow",
+            "--auth-token", authToken,
+            "--repo", "some repo",
+            "--run-id", "some run id",
+        };
+        await app.RunAsync(args);
+        var output = console.ReadAllAsString();
+
+        var settings = new VerifySettings();
+        settings.ScrubAppName();
+        await Verify(output, settings).UseParameters(scenario);
+    }
+
+    /// <summary>
+    /// Tests the validation of the --repo option for the <see cref="ReadDataFromDifferentGitHubWorkflowCommand"/> command.
+    /// </summary>
+    [Theory]
+    [InlineData("empty-string", "")]
+    [InlineData("white-space", "   ")]
+    public async Task RepoValidation(string scenario, string repo)
+    {
+        using var console = new FakeInMemoryConsole();
+        var app = new ShareDataBetweenJobsCli();
+        app.CliApplicationBuilder.UseConsole(console);
+        var args = new[]
+        {
+            "read-data-different-workflow",
+            "--auth-token", "some auth token",
+            "--repo", repo,
+            "--run-id", "some run id",
+        };
+        await app.RunAsync(args);
+        var output = console.ReadAllAsString();
+
+        var settings = new VerifySettings();
+        settings.ScrubAppName();
+        await Verify(output, settings).UseParameters(scenario);
+    }
+
+    /// <summary>
+    /// Tests the validation of the --run-id option for the <see cref="ReadDataFromDifferentGitHubWorkflowCommand"/> command.
+    /// </summary>
+    [Theory]
+    [InlineData("empty-string", "")]
+    [InlineData("white-space", "   ")]
+    public async Task RunIdValidation(string scenario, string runId)
+    {
+        using var console = new FakeInMemoryConsole();
+        var app = new ShareDataBetweenJobsCli();
+        app.CliApplicationBuilder.UseConsole(console);
+        var args = new[]
+        {
+            "read-data-different-workflow",
+            "--auth-token", "some auth token",
+            "--repo", "some repo",
+            "--run-id", runId,
+        };
+        await app.RunAsync(args);
+        var output = console.ReadAllAsString();
+
+        var settings = new VerifySettings();
+        settings.ScrubAppName();
+        await Verify(output, settings).UseParameters(scenario);
+    }
+
+    /// <summary>
+    /// Tests the validation of the --artifact-name option for the <see cref="ReadDataFromDifferentGitHubWorkflowCommand"/> command.
+    /// </summary>
+    [Theory]
+    [InlineData("empty-string", "")]
+    [InlineData("white-space", "   ")]
+    public async Task ArtifactNameValidation(string scenario, string artifactName)
     {
         using var console = new FakeInMemoryConsole();
         var app = new ShareDataBetweenJobsCli();
@@ -111,21 +191,23 @@ public class CliIntegrationTests
             "--auth-token", "some auth token",
             "--repo", "some repo",
             "--run-id", "some run id",
-            "--artifact-name", "  ",
+            "--artifact-name", artifactName,
         };
         await app.RunAsync(args);
         var output = console.ReadAllAsString();
 
         var settings = new VerifySettings();
         settings.ScrubAppName();
-        await Verify(output, settings);
+        await Verify(output, settings).UseParameters(scenario);
     }
 
     /// <summary>
-    /// Tests that the --data-filename option is must have a value for the <see cref="ReadDataFromDifferentGitHubWorkflowCommand"/> command.
+    /// Tests the validation of the --data-filename option for the <see cref="ReadDataFromDifferentGitHubWorkflowCommand"/> command.
     /// </summary>
-    [Fact]
-    public async Task ArtifactFilenameMustHaveValue()
+    [Theory]
+    [InlineData("empty-string", "")]
+    [InlineData("white-space", "   ")]
+    public async Task ArtifactFilenameValidation(string scenario, string artifactFilename)
     {
         using var console = new FakeInMemoryConsole();
         var app = new ShareDataBetweenJobsCli();
@@ -137,13 +219,13 @@ public class CliIntegrationTests
             "--repo", "some repo",
             "--run-id", "some run id",
             "--artifact-name", "some artifact name",
-            "--data-filename", "  ",
+            "--data-filename", artifactFilename,
         };
         await app.RunAsync(args);
         var output = console.ReadAllAsString();
 
         var settings = new VerifySettings();
         settings.ScrubAppName();
-        await Verify(output, settings);
+        await Verify(output, settings).UseParameters(scenario);
     }
 }
