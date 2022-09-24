cd53c223a660b5c8f18c59aa2e89a28b0e71dd39
Eduardo Serrano <eduardomserrano@outlook.com>
2022-09-24 13:53:30 +0100

    adding tests 14

---
+++ ShareJobsData/tests/ShareJobsDataCli.Tests/CliCommands/ReadDataCurrentWorkflow/ReadDataFromCurrentGitHubWorkflowCommandErrorTests.cs
@@ -0,0 +1,79 @@
+namespace ShareJobsDataCli.Tests.CliCommands.ReadDataCurrentWorkflow;
+
+/// <summary>
+/// These tests check what happens when a logic violation occurs when running the <see cref="ReadDataFromCurrentGitHubWorkflowCommand"/>.
+/// </summary>
+[Trait("Category", XUnitCategories.LogicFailure)]
+[Trait("Category", XUnitCategories.ReadDataFromCurrentGitHubWorkflowCommand)]
+[UsesVerify]
+public class ReadDataFromCurrentGitHubWorkflowCommandErrorTests
+{
+    /// <summary>
+    /// Tests that the <see cref="ReadDataFromCurrentGitHubWorkflowCommand"/> shows expected error message when it can't download
+    /// the specified artifact because it doesn't exist on the current workflow run.
+    /// </summary>
+    [Fact]
+    public async Task ArtifactNotFound()
+    {
+        var githubEnvironment = new TestsGitHubEnvironment();
+        using var testHttpMessageHandler = new TestHttpMessageHandler();
+        testHttpMessageHandler.MockListArtifactsFromCurrentWorkflowRun(builder =>
+        {
+            builder
+                .FromCurrentWorkflowRun(githubEnvironment.GitHubActionRuntimeUrl, githubEnvironment.GitHubActionRunId)
+                .WithResponseStatusCode(HttpStatusCode.OK)
+                .WithResponseContentFromFilepath(TestFiles.GetFilepath("list-artifacts.http-response.json"));
+        });
+        (var httpClient, var outboundHttpRequests) = TestHttpClientFactory.Create(testHttpMessageHandler);
+
+        var command = new ReadDataFromCurrentGitHubWorkflowCommand(httpClient, githubEnvironment)
+        {
+            ArtifactName = "not-gonna-find-it",
+            ArtifactFilename = "job-data.json",
+        };
+        using var console = new FakeInMemoryConsole();
+        var exception = await Should.ThrowAsync<CommandException>(() => command.ExecuteAsync(console).AsTask());
+
+        await Verify(exception.Message).AppendToMethodName("console-output");
+        await Verify(outboundHttpRequests).AppendToMethodName("outbound-http");
+    }
+
+    /// <summary>
+    /// Tests that the <see cref="ReadDataFromCurrentGitHubWorkflowCommand"/> shows expected error message when it can't download
+    /// the specified artifact file because it doesn't exist on the downloaded artifact from the current workflow run.
+    /// </summary>
+    [Fact]
+    public async Task ArtifactFileNotFound()
+    {
+        var githubEnvironment = new TestsGitHubEnvironment();
+        using var testHttpMessageHandler = new TestHttpMessageHandler();
+        testHttpMessageHandler.MockListArtifactsFromCurrentWorkflowRun(builder =>
+        {
+            builder
+                .FromCurrentWorkflowRun(githubEnvironment.GitHubActionRuntimeUrl, githubEnvironment.GitHubActionRunId)
+                .WithResponseStatusCode(HttpStatusCode.OK)
+                .WithResponseContentFromFilepath(TestFiles.GetFilepath("list-artifacts.http-response.json"));
+        });
+        testHttpMessageHandler.MockGetContainerItemsFromCurrentWorkflowRun(builder =>
+        {
+            builder
+                .FromFileContainerResourceUrl(
+                    fileContainerResourceUrl: "https://pipelines.actions.githubusercontent.com/pasYWZMKAGeorzjszgve9v6gJE03WMQ2NXKn6YXBa7i57yJ5WP/_apis/resources/Containers/2535982",
+                    artifactName: "job-data")
+                .WithResponseStatusCode(HttpStatusCode.OK)
+                .WithResponseContentFromFilepath(TestFiles.GetFilepath("get-container-items.http-response.json"));
+        });
+        (var httpClient, var outboundHttpRequests) = TestHttpClientFactory.Create(testHttpMessageHandler);
+
+        var command = new ReadDataFromCurrentGitHubWorkflowCommand(httpClient, githubEnvironment)
+        {
+            ArtifactName = "job-data",
+            ArtifactFilename = "not-gonna-find-me.json",
+        };
+        using var console = new FakeInMemoryConsole();
+        var exception = await Should.ThrowAsync<CommandException>(() => command.ExecuteAsync(console).AsTask());
+
+        await Verify(exception.Message).AppendToMethodName("console-output");
+        await Verify(outboundHttpRequests).AppendToMethodName("outbound-http");
+    }
+}
