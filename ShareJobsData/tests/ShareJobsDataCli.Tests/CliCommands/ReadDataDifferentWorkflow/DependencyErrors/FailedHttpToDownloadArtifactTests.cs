6fc982244ad83ea16a8549d68c22cfb5e027169c
Eduardo Serrano <eduardomserrano@outlook.com>
2022-09-25 15:10:55 +0100

    adding tests 21

--- ShareJobsData/tests/ShareJobsDataCli.Tests/CliCommands/ReadDataDifferentWorkflow/DependencyErrors/FailedHttpToDownloadArtifactTests.cs
+++ ShareJobsData/tests/ShareJobsDataCli.Tests/CliCommands/ReadDataDifferentWorkflow/DependencyErrors/FailedHttpToDownloadArtifactTests.cs
@@ -48,4 +48,47 @@ public class FailedHttpToDownloadArtifactTests
         await Verify(exception.Message).AppendToMethodName("console-output");
         await Verify(outboundHttpRequests).AppendToMethodName("outbound-http");
     }
+
+    /// <summary>
+    /// Tests that the <see cref="ReadDataFromCurrentGitHubWorkflowCommand"/> shows expected error message when the
+    /// content of the downloaded artifact is not JSON.
+    /// </summary>
+    [Fact]
+    public async Task ArtifactContentIsNotJson()
+    {
+        const string repoName = "edumserrano/share-jobs-data";
+        const string runId = "test-run-id";
+        using var testHttpMessageHandler = new TestHttpMessageHandler();
+        testHttpMessageHandler.MockListArtifactsFromDifferentWorkflowRun(builder =>
+        {
+            builder
+                .FromWorkflowRun(repoName, runId)
+                .WithResponseStatusCode(HttpStatusCode.OK)
+                .WithResponseContentFromFilepath(TestFiles.GetFilepath("list-artifacts.http-response.json"));
+        });
+        testHttpMessageHandler.MockDownloadArtifactFromDifferentWorkflowRun(builder =>
+        {
+            builder
+                .FromWorkflowArtifactId(repoName, artifactId: "351670722")
+                .WithResponseStatusCode(HttpStatusCode.OK)
+                .WithResponseContentFromFilepath(TestFiles.GetFilepath("download-artifact.http-response.zip"));
+        });
+        (var httpClient, var outboundHttpRequests) = TestHttpClientFactory.Create(testHttpMessageHandler);
+        var githubEnvironment = new TestsGitHubEnvironment();
+
+        var command = new ReadDataFromDifferentGitHubWorkflowCommand(httpClient, githubEnvironment)
+        {
+            AuthToken = "auth-token",
+            Repo = repoName,
+            RunId = runId,
+            ArtifactName = "job-data",
+            ArtifactFilename = "job-data.json",
+        };
+        using var console = new FakeInMemoryConsole();
+        var exception = await Should.ThrowAsync<CommandException>(() => command.ExecuteAsync(console).AsTask());
+
+        var output = console.ReadOutputString();
+        await Verify(exception.Message).AppendToMethodName("console-output");
+        await Verify(outboundHttpRequests).AppendToMethodName("outbound-http");
+    }
 }
