fd636f0e0fbc8f7e0f2685c37474dfb76a67fdde
Eduardo Serrano <eduardomserrano@outlook.com>
2023-01-15 15:53:34 +0000

    test using github output file 11

--- ShareJobsData/src/ShareJobsDataCli/ShareDataBetweenJobsCli.cs
+++ ShareJobsData/src/ShareJobsDataCli/ShareDataBetweenJobsCli.cs
@@ -1,12 +1,20 @@
+using System.IO;
+
 namespace ShareJobsDataCli;
 
 public class ShareDataBetweenJobsCli
 {
     public ShareDataBetweenJobsCli()
     {
+        var githubOutputFile = Environment.GetEnvironmentVariable("GITHUB_OUTPUT") ?? string.Empty;
+        var textWriter = new StreamWriter(githubOutputFile, append: true, Encoding.UTF8);
+        textWriter.AutoFlush = true;
+        Console.SetOut(textWriter);
+
         CliApplicationBuilder = new CliApplicationBuilder()
             .AddCommandsFromThisAssembly()
-            .UseConsole(new GitHubStepOutputConsole());
+            //.UseConsole(new GitHubStepOutputConsole())
+            ;
     }
 
     public CliApplicationBuilder CliApplicationBuilder { get; }
