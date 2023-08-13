9cdc60615b008010ffa40b65835ef2bfff95bb9f
Eduardo Serrano <eduardomserrano@outlook.com>
2023-08-13 16:52:30 +0100

    Identify the `Curiously recurring template pattern` with documentation

--- ShareJobsData/tests/ShareJobsDataCli.Tests/Auxiliary/Http/GitHubHttpClient/BaseResponseMockBuilder.cs
+++ ShareJobsData/tests/ShareJobsDataCli.Tests/Auxiliary/Http/GitHubHttpClient/BaseResponseMockBuilder.cs
@@ -1,5 +1,26 @@
 namespace ShareJobsDataCli.Tests.Auxiliary.Http.GitHubHttpClient;
 
+// The signature `BaseResponseMockBuilder<T> where T : BaseResponseMockBuilder<T>`
+// looks weird at first sight right? 
+//
+// Here are a few links that help explain the benefits of implementing `A<T> : A<T>` and using the
+// `return (T)this;` in the base class:
+// - https://www.codeproject.com/Articles/240756/Hierarchically-Implementing-the-Bolchs-Builder-Pat
+// - https://code-maze.com/fluent-builder-recursive-generics/
+// - https://www.appsloveworld.com/csharp/100/192/chaining-methods-in-base-and-derived-class
+// - https://github.com/dotnet/csharplang/issues/2495
+//
+// Apparently this pattern has a name, it's called the `Curiously recurring template pattern`:
+// - http://www.codecutout.com/blog/curiously-repeating-template-pattern/
+//
+// In short:
+// It looks weird at first but it enables a fluent like API with a generic
+// but the `return (T)this;` allows the chaining to be specifically tied to
+// the generic type T.
+//
+// See all the types that implement BaseResponseMockBuilder<T>. All of these types
+// have access to the methods in the base class but when chaining you will also have
+// access to the methods specific to the generic T type.
 internal abstract class BaseResponseMockBuilder<T>
     where T : BaseResponseMockBuilder<T>
 {
